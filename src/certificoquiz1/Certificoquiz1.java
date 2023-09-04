9119dd26f9ed4f2cb81c29d72bd53e4faf35c1e7
khian <khian@khiannecert19>
2023-09-04 16:43:38 +0800

    Version 1

--- src/certificoquiz1/Certificoquiz1.java
+++ src/certificoquiz1/Certificoquiz1.java
@@ -15,7 +15,7 @@ public class Certificoquiz1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hi World");
     }
     
 }
