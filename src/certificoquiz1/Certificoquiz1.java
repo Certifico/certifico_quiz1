0a24ed1bd2c388b37207476344f749f80d3679d3
khian <khian@khiannecert19>
2023-09-05 17:26:01 +0800

    QUIZ 1 VERSION 1

--- src/certificoquiz1/Certificoquiz1.java
+++ src/certificoquiz1/Certificoquiz1.java
@@ -9,13 +9,102 @@ package certificoquiz1;
  *
  * @author khian
  */
-public class Certificoquiz1 {
+import java.util.Scanner;
+import java.util.Arrays;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Certificoquiz1 {
+        
+    public static Scanner scn = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        System.out.println("Hi World");
+        displayniSiya();
+        TheMenu();
+}
+    public static void displayniSiya(){
+        System.out.println("***************************************");
+        System.out.println("* KHIANNE QUIZ 1, Part 2: Code Time !!*");
+        System.out.println("***************************************");
+    }
+    public static void TheMenu(){
+        System.out.println("PRESS [1] Question 1 Answer");
+        System.out.println("PRESS [2] Question 2 Answer");
+        System.out.println("PRESS [3] Question 3 Answer");
+        System.out.println("PRESS [4] EXIT");
+        int choice = userChoice(1, 4);
+        
+        switch (choice) {
+            case 1:
+                number1();
+                break;
+            case 2:
+                number2();
+                break;
+            case 3:
+                number3();
+                break;
+            case 4:
+                scn.close();
+                System.out.println("THANK YOU SIR");
+                break;
+            default:
+                System.out.println("TRY AGAIN.");
+                break;
+        }
     }
     
-}
+    public static void number1(){
+      int [] arr = {82,44,96,7,13,56,74,14,98,36};
+        System.out.println(Arrays.toString(arr));
+    }
+    
+    public static void number2(){
+    ////Sorry Sir wako ka study ani
+    }
+    
+    public static void number3(){
+    
+            int[] arrey = {82,44,96,7,13,56,74,14,98,36};
+    
+        System.out.println("Original Set of Array: ");
+        printArray(arrey);
+        
+        bubbleSort(arrey);
+        
+        System.out.println("New Set of Array: ");
+        printArray(arrey);
+        
+    }
+    public static void bubbleSort(int arrey[]){
+        int n = arrey.length;
+        
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arrey [j] > arrey [j + 1]) {
+                    int m = arrey[j];
+                    arrey[j]= arrey[j + 1];
+                    arrey[j + 1] = m;
+                    
+                }
+            }
+        }
+        
+    }
+    public static void printArray(int arrey[]){
+        for (int num : arrey) {
+            System.out.println(num + " ");
+        }
+    } 
+
+    private static int userChoice(int min, int max) {
+        int choice;
+        while (true) {            
+            System.out.println("Enter your Choice: ");
+            choice = scn.nextInt();
+            if (choice >= min && choice <= max) {
+                break;
+            }
+            System.out.println("TRY AGAIN.");
+        }
+        return choice;
+    }
+    }
